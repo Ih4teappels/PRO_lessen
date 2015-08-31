4e10bf6339b1024423e4dbb2928be4b2903a8b6d
Ih4teappels <kev10@live.nl>
2015-08-31 15:06:08 +0200

    dit is een push kek

---
+++ script.js
@@ -0,0 +1,15 @@
+var counter = 0;
+
+window.addEventListener("load",function(){
+
+	var images = ["img/Photo1.jpg","img/Photo2.jpg","img/Photo3.jpg","img/Photo4.jpg","img/Photo5.jpg","img/Photo6.jpg"];
+	
+
+});
+
+window.addEventListener("click",function(images){
+
+	counter++;
+	console.log(counter);
+
+});
