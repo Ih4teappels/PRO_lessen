041e3f1857f39a01205febd0bd34231b4436b0b3
Ih4teappels <kev10@live.nl>
2015-09-15 16:08:11 +0200

    alles

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 var counter = 0;
+var images = ["img/Photo1.jpg","img/Photo2.jpg","img/Photo3.jpg","img/Photo4.jpg","img/Photo5.jpg","img/Photo6.jpg"];
 
 window.addEventListener("load",function(){
 
-	var images = ["img/Photo1.jpg","img/Photo2.jpg","img/Photo3.jpg","img/Photo4.jpg","img/Photo5.jpg","img/Photo6.jpg"];
+	var galleryImage = document.querySelector("#galleryImage");
+
+	document.querySelector("#left").addEventListener("click",function(){
+	var url = images[counter];
+	counter--;
+		if(counter == -1) {
+			counter = 4;
+		}
+
+		
+		galleryImage.src = url;
+	});
+
+	document.querySelector("#right").addEventListener("click",function(){
+	var url = images[counter];
+	counter++;
+		if(counter == 5) {
+			counter = 0;
+		}
+
+		galleryImage.src = url;
+	});
+	
 	
 
 });
 
-window.addEventListener("click",function(images){
+	
+	
 
-	counter++;
-	console.log(counter);
 
-});
+
+
